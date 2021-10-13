23b4373b4e1c2b1100d0f2ee982e519086521005
Takahiro Kato <dozo13189@gmail.com>
2021-10-14 02:38:27 +0900

    aboutページを作成

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div>
     <h1>dozo13189@gmail.com</h1>
   </div>
 </template>
---
+++ src/views/Administrator.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>選手名の日本語発音の統一が目的です。</p>
+  </div>
+</template>
