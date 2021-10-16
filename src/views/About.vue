c2ff1e36a4f438942ed8b64b1d67ee548e010745
Takahiro Kato <dozo13189@gmail.com>
2021-10-16 21:41:59 +0900

    aboutページの文字を中央寄せ

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,17 @@
 <template>
 
-  <div class="m-5">
+  <div class="m-5 has-text-centered">
       <h1 class="is-size-4 has-text-weight-bold has-text-black">目的</h1>
       <p class="has-text-black">選手名のカタカナ表記の統一が目的です。</p>
   </div>
 
-  <div class="m-5">
+  <div class="m-5 has-text-centered">
     <h1 class="is-size-4 has-text-weight-bold has-text-black">運営</h1>
       <p class="has-text-black">メンバーに掲載されている人が、日本語でのカタカナ表記を決めています。</p>
       <p class="has-text-black">運営に参加したい方は、メンバーにご連絡下さい。</p>
   </div>
 
-  <div class="m-5">
+  <div class="m-5 has-text-centered">
     <h1 class="is-size-4 has-text-weight-bold has-text-black">メンバー</h1>
       <p class="has-text-black">dozo13189@gmail.com</p>
   </div>
